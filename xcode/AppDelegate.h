2e586793abce55be4aace090a3a95469ebe8b0d3
Antti Peltola <antti.peltola@kolumbus.fi>
2020-10-07 20:12:25 +0300

    Removed all test code.

---
+++ xcode/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  xcode
+//
+//  Created by ajp on 07.10.2020.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ xcode/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  xcode
+//
+//  Created by ajp on 07.10.2020.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
